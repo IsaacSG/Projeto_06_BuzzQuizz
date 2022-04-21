eb4953ea09c8bd3e367a23f6d59b16bdcfb2222c
IsaacSG <isaacgraciano123@gmail.com>
2022-04-20 23:39:05 -0300

    Renderizar respostas

--- JS/Script_TelaResponderQuizz.js
+++ JS/Script_TelaResponderQuizz.js
@@ -1,4 +1,5 @@
 let id;
+let respostas = [];
 function responderQuizz(elemento){
     const pagina01 = document.querySelector(".pagina01");
     pagina01.classList.add("oculto");
@@ -14,10 +15,24 @@ function renderizarQuizzSelecionado () {
 function responderQuizzSelecionado (response) {
     let quizz = response.data;
     let quizzSelecionado;
+    let respostasQuizz;
     for(i = 0; i < quizz.length; i++){
         if (quizz[i].id == id) {
-        quizzSelecionado = document.querySelector(".perguntasQuizz");
-        quizzSelecionado.innerHTML += `<div class ="perguntas"><p>${quizz[i].questions[i].title} </p></div>`
+            quizzSelecionado = document.querySelector(".perguntasQuizz");
+            let questoes = quizz[0].questions;
+            for(o = 0; o < questoes.length; o++){
+                quizzSelecionado.innerHTML += `<div class ="perguntas${o}"><p class = "titulo">${quizz[i].questions[o].title} </p> </div>`
+                respostas = quizz[i].questions[o].answers;
+                respostas.sort(misturar);
+                for(p = 0; p < respostas.length; p++){
+                    respostasQuizz = document.querySelector(`.perguntas${o}`);
+                    respostasQuizz.innerHTML += `<div class = "respostas"><img src = "${respostas[p].image}"/> <p>${respostas[p].text}</p></div>`;
+                    console.log(respostas)
+                }
+            }
         }
     }
+}
+function misturar(){
+    return Math.random() - 0.5;
 }
