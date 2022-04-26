a4ff69e10fcd69419d51f5abb0e9ef546c3e9a5c
IsaacSG <isaacgraciano123@gmail.com>
2022-04-26 00:15:29 -0300

    Exibição de um quizz

--- JS/Script_TelaResponderQuizz.js
+++ JS/Script_TelaResponderQuizz.js
@@ -1,6 +1,6 @@
 let id;
 let respostas = [];
-function responderQuizz(elemento){
+function carregarQuizz(elemento){
     const pagina01 = document.querySelector(".pagina01");
     pagina01.classList.add("oculto");
     const pagina02 = document.querySelector(".pagina02");
@@ -10,31 +10,52 @@ function responderQuizz(elemento){
 }
 function renderizarQuizzSelecionado () {
     const promisse = axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes");
-    promisse.then(responderQuizzSelecionado);
+    promisse.then(carregarQuizzSelecionado);
 }
-function responderQuizzSelecionado (response) {
+function carregarQuizzSelecionado (response) {
     let quizz = response.data;
     let quizzSelecionado;
     let respostasQuizz;
+    let editarcss;
+    let cor;
+    let imagemFundo;
     for(i = 0; i < quizz.length; i++){
         if (quizz[i].id == id) {
             quizzSelecionado = document.querySelector(".imagemTopo");
-            quizzSelecionado.innerHTML += `<div class ="topoImagem"> <p>${quizz[i].title}</p> <img src = "${quizz[i].image}"/></div>`
+            editarcss = document.querySelector(".imagemTopo");
+            imagemFundo = `${quizz[i].image}`
+            editarcss.style.backgroundImage = `url("${imagemFundo}")`;
+            quizzSelecionado.innerHTML += `<div class ="topoImagem"> <p>${quizz[i].title}</p></div>`;
             let questoes = quizz[0].questions;
             quizzSelecionado = document.querySelector(".perguntasQuizz");
             for(o = 0; o < questoes.length; o++){
-                quizzSelecionado.innerHTML += `<div class ="perguntas${o}"><p class = "tituloPergunta">${quizz[i].questions[o].title} </p> </div>`
+                quizzSelecionado.innerHTML += `<div class ="perguntas${o}"><p class = "tituloPergunta">${quizz[i].questions[o].title} </p> </div>`;
+                cor = `${quizz[i].questions[o].color}`;
+                editarcss = document.querySelector(`.perguntas${o} > .tituloPergunta`)
+                editarcss.style.backgroundColor = cor;
                 respostas = quizz[i].questions[o].answers;
                 respostas.sort(misturar);
                 for(p = 0; p < respostas.length; p++){
                     respostasQuizz = document.querySelector(`.perguntas${o}`);
-                    respostasQuizz.innerHTML += `<div class = "respostas"><img src = "${respostas[p].image}"/> <p>${respostas[p].text}</p></div>`;
-                    console.log(respostas)
+                    respostasQuizz.innerHTML += `<div class = "respostas${o}${p}" id="${respostas[p].isCorrectAnswer}" onclick="responderQuizz(this)"><img src = "${respostas[p].image}"/> <p>${respostas[p].text}</p></div>`;
                 }
             }
         }
     }
 }
+
+/*function responderQuizz (elemento){
+    let editarcss;
+    let alternativa = elemento.className;
+    id = elemento.id;
+    let verde = "rgb(23, 255, 0)";
+    let vermelho = "rgb(255,0,0)";
+    if(id == true){
+        editarcss = document.querySelector(`".${alternativa}" > p`);
+        editarcss.style.color = verde;
+    }
+}*/
+
 function misturar(){
     return Math.random() - 0.5;
 }
