734c3fec19c11b8f42c12ccd4b055f264f3a5727
IsaacSG <isaacgraciano123@gmail.com>
2022-04-26 17:00:34 -0300

    Função de resposta

--- JS/Script_TelaResponderQuizz.js
+++ JS/Script_TelaResponderQuizz.js
@@ -1,5 +1,6 @@
 let id;
 let respostas = [];
+let contador = 0;
 function carregarQuizz(elemento){
     const pagina01 = document.querySelector(".pagina01");
     pagina01.classList.add("oculto");
@@ -29,7 +30,7 @@ function carregarQuizzSelecionado (response) {
             let questoes = quizz[0].questions;
             quizzSelecionado = document.querySelector(".perguntasQuizz");
             for(o = 0; o < questoes.length; o++){
-                quizzSelecionado.innerHTML += `<div class ="perguntas${o}"><p class = "tituloPergunta">${quizz[i].questions[o].title} </p> </div>`;
+                quizzSelecionado.innerHTML += `<ul class ="perguntas${o}"><p class = "tituloPergunta">${quizz[i].questions[o].title} </p> </ul>`;
                 cor = `${quizz[i].questions[o].color}`;
                 editarcss = document.querySelector(`.perguntas${o} > .tituloPergunta`)
                 editarcss.style.backgroundColor = cor;
@@ -37,24 +38,35 @@ function carregarQuizzSelecionado (response) {
                 respostas.sort(misturar);
                 for(p = 0; p < respostas.length; p++){
                     respostasQuizz = document.querySelector(`.perguntas${o}`);
-                    respostasQuizz.innerHTML += `<div class = "respostas${o}${p}" id="${respostas[p].isCorrectAnswer}" onclick="responderQuizz(this)"><img src = "${respostas[p].image}"/> <p>${respostas[p].text}</p></div>`;
+                    respostasQuizz.innerHTML += `<li class = "respostas${o}${p}" id="${respostas[p].isCorrectAnswer}" onclick="responderQuizz(this)"><img src = "${respostas[p].image}"/> <p>${respostas[p].text}</p></li>`;
                 }
             }
         }
     }
 }
 
-/*function responderQuizz (elemento){
+function responderQuizz (elemento){
     let editarcss;
     let alternativa = elemento.className;
     id = elemento.id;
     let verde = "rgb(23, 255, 0)";
     let vermelho = "rgb(255,0,0)";
-    if(id == true){
-        editarcss = document.querySelector(`".${alternativa}" > p`);
+    let elementoPai;
+
+    if(id == "true"){
+        editarcss = document.querySelector(`.${alternativa} > p`);
         editarcss.style.color = verde;
+        elementoPai = document.querySelector(`.${elemento.parentNode.className}`);
+        elementoPai.classList.add("opacidade");
+        contador = contador + 1;
     }
-}*/
+    else {
+        elementoPai = document.querySelector(`.${elemento.parentNode.className}`);
+        elementoPai.classList.add("opacidade");
+        editarcss = document.querySelector(`.${alternativa} > p`);
+        editarcss.style.color = vermelho;
+    }
+}
 
 function misturar(){
     return Math.random() - 0.5;
